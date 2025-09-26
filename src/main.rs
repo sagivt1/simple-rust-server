a2e1fadcd2164747c06f3f4c10a5fa3993dc5150
sagivt1 <sagiv.talker@gmail.com>
2025-09-26 15:47:43 +0300

    Server bind and accepts connections

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,35 @@
+use std::net::TcpListener;
+
+// Main function for the web server
 fn main() {
-    println!("Hello, world!");
+    // Define address to bind "127.0.0.1:7878" (localhost) with port 7878.
+    let listener = match TcpListener::bind("127.0.0.1:7878"){
+        Ok(listener) => {
+            // if the bind succsued get TcpListener
+            println!("Server listening on http://127.0.0.1:7878");
+            listener
+        }
+        Err(e) => {
+            // if bind fails print the error message and panic
+            eprintln!("Failed to bind to address: {}", e);
+            panic!("Could not start the server, Exiting");
+        }
+    };
+
+    // Wait for connection -> when recive connection return Result<TcpStream, E>
+    for stream in listener.incoming() {
+        // If connection succeseful, get TcpStream
+        // if connection fails, panic and crash
+        match stream {
+            Ok(stream) => {
+                println!("Connection established!");
+                // todo: Handel Connection 
+            }
+            Err(e) => {
+                // log the error and continue listening for the next connection
+                println!("Connection failed: {}", e);
+            }
+        }
+    }
+
 }
